c121cdec02f915fab67c137d104b26cb51de0111
mohamedwaelbenismail <mohamedwaelbenismail@gmail.com>
2020-03-21 18:36:39 +0100

    deploy

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome');
-}).listen(80, "0.0.0.0");
-console.log('Server running');

c2bbff92ed9a9c31529e86b840d9dc95ec7dc544
mohamedwaelbenismail <mohamedwaelbenismail@gmail.com>
2020-03-21 13:31:44 +0100

    correction

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Welcome');
-}).listen(3001, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3001/');
+}).listen(3000, "0.0.0.0");
+console.log('Server running');
